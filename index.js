86f110e65e38b162aa163e3e98194f9e468020c4
GilgameshEU <d1vergence@yandex.ru>
2023-03-09 18:46:54 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://main.d3gmh33f70aijh.amplifyapp.com"], methods: ["GET", "POST", "DELETE", "PUT"] }));
-// app.options("*", cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(router);
 
 app.listen(5000, () => {
